c1de240c6a97f2d91cf5c9db8959eb5167bdce06
Рамиль Шарапов <cuparario@yandex.ru>
2023-11-27 17:18:53 +0300

    Домашнее задание 2

---
+++ Task_01/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Задача 1: Напишите программу, которая принимает на вход число 
+и проверяет, кратно ли оно одновременно 7 и 23. */
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+string result = (num % 7 == 0 && num % 23 == 0) ? "да" : "нет";
+System.Console.WriteLine(result);
---
+++ Task_03/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 3: Напишите программу, которая принимает на вход целое число 
+из отрезка [10, 99] и показывает наибольшую цифру числа. */
+
+
+System.Random numSintezator = new System.Random();
+
+int rndNum = numSintezator.Next(10, 100);
+
+Console.WriteLine("Случайное число: " + rndNum);
+
+int digit1 = rndNum / 10;
+
+int digit2 = rndNum % 10;
+
+if (digit1 > digit2)
+{
+    Console.WriteLine("Цифра 1 больше цифры 2: " + digit1 + " " + digit2);
+}
+else
+{
+    Console.WriteLine("Цифра 2 больше цифры 1: " + digit1 + " " + digit2);
+}
